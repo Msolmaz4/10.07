e9bc12ced3895d686a58bf62d024505fb09bee4f
muhammet solmaz <msolmaz83@gmail.com>
2021-07-10 21:07:46 +0200

    wer schon

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import Card from './components/Card';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App =()=> {
+    return (
+    
+        <div>
+            <div className="card-group">
+                <Card cardTitle='Iphone' />
+                    
+                <Card cardTitle='Samsung' />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+                <Card  cardTitle='Oppo'/>  
+                        </div>
+                    </div>
+                    )
+}
+
+                    ReactDOM.render(
+
+                    <App />,
+                    document.getElementById('root')
+                    );
