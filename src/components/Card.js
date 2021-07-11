5fda1a13f43f0be373c0314173f0e43d1063d200
muhammet solmaz <msolmaz83@gmail.com>
2021-07-11 02:42:11 +0200

    state

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
+class Card extends React.Component {
+    render() {
+        return (
+            <div className="card">
+                <img src={this.props.image} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{this.props.cardTitle}</h5>
+                    <p className="card-text">{this.props.cardText}</p>
+                    <p className="card-text"><small className="text-muted">{this.props.updatedTime}</small></p>
+                </div>
+            </div>
+        );
+    }
 
-const Card = (props) => {
- return (
+}
 
-     <div className="card">
-       <img src="..." className="card-img-top" alt="..."/>
-        <div className="card-body">
-            <h5 className="card-title">{props.cardTitle}</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-  </div>
+Card.defaultProps ={
+    cardTitle:'Deafault Card Title'
+}
 
 
- );
-};
 export default Card;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+
 import Card from './components/Card';
+import Collapse from './components/Collapse';
 
-const App =()=> {
+const App = () => {
     return (
-    
-        <div>
-            <div className="card-group">
-                <Card cardTitle='Iphone' />
-                    
-                <Card cardTitle='Samsung' />
 
-                <Card  cardTitle='Oppo'/>  
-                        </div>
+        <div className='contanier'>
+            <div className="card-group w-100">
+                <div className='row'>
+                    <div className='col'>
+                        <Collapse href="collapseExample1">
+
+                            <Card cardTitle='Card Tiitle'
+                                cardText='lorem'
+                                updatedTime='last Update 1 min '
+                                image='https://picsum.photos/id/1/200/300' />
+
+                        </Collapse>
+
                     </div>
-                    )
+                    <div className='col'>
+                        <Collapse href="collapseExample2">
+
+                            <Card cardTitle='Card Tiitle'
+                                cardText='lorem'
+                                updatedTime='last Update 1 min '
+                                image='https://picsum.photos/id/2/200/300' />
+
+                        </Collapse>
+
+                    </div>
+                    <div className='col'>
+                        <Collapse href="collapseExample3">
+
+                            <Card cardTitle='Card Tiitle'
+                                cardText='lorem'
+                                updatedTime='last Update 1 min '
+                                image='https://picsum.photos/id/3/200/300' />
+
+                        </Collapse>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+    )
 }
 
-                    ReactDOM.render(
+ReactDOM.render(
 
-                    <App />,
-                    document.getElementById('root')
-                    );
+    <App />,
+    document.getElementById('root')
+);
